ea36ff47c4d7c6bfe10c3c1ba54e68b7ffc82ced
Sam Schreiber <sam.schreiber@gmail.com>
2011-03-23 07:31:28 +0000

    Add some basic sanity checks to the inputs to the spectator server.

--- src/ggp/spectator/GGP_SpectatorServlet.java
+++ src/ggp/spectator/GGP_SpectatorServlet.java
@@ -12,6 +12,8 @@ import javax.jdo.PersistenceManager;
 import javax.servlet.http.*;
 
 import com.google.appengine.api.channel.ChannelServiceFactory;
+import com.google.appengine.repackaged.org.json.JSONException;
+import com.google.appengine.repackaged.org.json.JSONObject;
 
 @SuppressWarnings("serial")
 public class GGP_SpectatorServlet extends HttpServlet {
@@ -143,9 +145,13 @@ public class GGP_SpectatorServlet extends HttpServlet {
         PersistenceManager pm = Persistence.getPersistenceManager();
         try {
             theMatch = pm.detachCopy(pm.getObjectById(MatchData.class, MatchData.getKeyFromJSON(theMatchJSON)));
+            performUpdateValidationChecks(theMatch.getMatchJSON(), theMatchJSON);
+            performInternalConsistencyChecks(theMatchJSON);
             theMatch.setMatchJSON(theMatchJSON);
             pm.makePersistent(theMatch);
         } catch(JDOObjectNotFoundException e) {
+            performCreationValidationChecks(theMatchJSON);
+            performInternalConsistencyChecks(theMatchJSON);
             theMatch = new MatchData(theMatchJSON, theAuthToken);;
         } finally {
             pm.close();
@@ -165,5 +171,103 @@ public class GGP_SpectatorServlet extends HttpServlet {
         resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
         resp.setHeader("Access-Control-Allow-Headers", "*");
         resp.setHeader("Access-Control-Allow-Age", "86400");    
+    }
+    
+    // =======================================================================
+    // Validation checks, to make sure bad information is not published to the
+    // spectator server by a malicious adversary or malfunctioning system. These
+    // are not comprehensive, but are intended to provide basic sanity guarantees
+    // so that we aren't storing total nonsense in the spectator server.
+    
+    public void performCreationValidationChecks(String theJSON) throws IOException {
+        try {
+            JSONObject newMatchJSON = new JSONObject(theJSON);
+            
+            long startTime = newMatchJSON.getLong("startTime");
+            if (startTime < 1000000000000L || startTime > 2000000000000L) {
+                throw new IOException("Unreasonable start time.");
+            }
+            
+            String randomToken = newMatchJSON.getString("randomToken");
+            if (randomToken.length() < 12) {
+                throw new IOException("Random token is too short.");
+            }
+        } catch(JSONException e) {
+            throw new IOException("Could not parse JSON: " + e.toString());
+        }
+    }
+    
+    public void performUpdateValidationChecks(String oldJSON, String newJSON) throws IOException {
+        try {
+            JSONObject oldMatchJSON = new JSONObject(oldJSON);
+            JSONObject newMatchJSON = new JSONObject(newJSON);
+            
+            verifyEquals(oldMatchJSON, newMatchJSON, "matchId");
+            verifyEquals(oldMatchJSON, newMatchJSON, "startTime");
+            verifyEquals(oldMatchJSON, newMatchJSON, "randomToken");
+            verifyEquals(oldMatchJSON, newMatchJSON, "matchHostPK");
+            verifyEquals(oldMatchJSON, newMatchJSON, "startClock");
+            verifyEquals(oldMatchJSON, newMatchJSON, "playClock");                
+            verifyEquals(oldMatchJSON, newMatchJSON, "gameMetaURL");
+            verifyEquals(oldMatchJSON, newMatchJSON, "gameName");
+            verifyEquals(oldMatchJSON, newMatchJSON, "gameRulesheetHash");
+            
+            if (oldMatchJSON.has("isCompleted") && newMatchJSON.has("isCompleted") && oldMatchJSON.getBoolean("isCompleted") && !newMatchJSON.getBoolean("isCompleted")) {
+                throw new IOException("Cannot transition from completed to not-completed.");
+            }
+            if (oldMatchJSON.getJSONArray("states").length() > newMatchJSON.getJSONArray("states").length()) {
+                throw new IOException("Number of states decreased from " + oldMatchJSON.getJSONArray("states").length() + " to " + newMatchJSON.getJSONArray("states").length() + " during update: not allowed.");
+            }
+        } catch(JSONException e) {
+            throw new IOException("Could not parse JSON: " + e.toString());
+        }
+    }
+    
+    public void performInternalConsistencyChecks(String theJSON) throws IOException {
+        try {
+            JSONObject theMatchJSON = new JSONObject(theJSON);
+            
+            verifyHas(theMatchJSON, "matchId");
+            verifyHas(theMatchJSON, "startTime");
+            verifyHas(theMatchJSON, "randomToken");
+            verifyHas(theMatchJSON, "startClock");
+            verifyHas(theMatchJSON, "playClock");
+            verifyHas(theMatchJSON, "states");
+            verifyHas(theMatchJSON, "moves");
+            verifyHas(theMatchJSON, "stateTimes");
+            verifyHas(theMatchJSON, "gameMetaURL");
+            
+            int movesLength = theMatchJSON.getJSONArray("moves").length();
+            int statesLength = theMatchJSON.getJSONArray("states").length();
+            int stateTimesLength = theMatchJSON.getJSONArray("stateTimes").length();
+            if (statesLength != stateTimesLength) {
+                throw new IOException("There are " + statesLength + " states, but " + stateTimesLength + " state times. Inconsistent!");
+            }
+            if (statesLength != movesLength+1) {
+                throw new IOException("There are " + statesLength + " states, but " + movesLength + " moves. Inconsistent!");
+            }
+        } catch(JSONException e) {
+            throw new IOException("Could not parse JSON: " + e.toString());
+        }
+    }
+    
+    public void verifyHas(JSONObject obj, String k) throws IOException {
+        if (!obj.has(k)) {
+            throw new IOException("Could not find required field " + k);
+        }
+    }
+    
+    public void verifyEquals(JSONObject old, JSONObject newer, String k) throws IOException {
+        try {
+            if (!old.has(k) && !newer.has(k)) {
+                return;
+            } else if (!old.has(k)) {
+                throw new IOException("Incompability for " + k + ": old has null, new has [" + newer.get(k) + "].");
+            } else if (!old.get(k).equals(newer.get(k))) {
+                throw new IOException("Incompability for " + k + ": old has [" + old.get(k) + "], new has [" + newer.get(k) + "].");
+            }
+        } catch (JSONException e) {
+            throw new IOException("JSON parsing exception: " + e.toString());
+        }
     }    
 }
