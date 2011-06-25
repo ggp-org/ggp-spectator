16a99ee5e7b7ba060d676524cc8b0a965de07aba
Sam Schreiber <sam.schreiber@gmail.com>
2011-06-25 01:59:51 +0000

    Add a prototype mapper that pushes out backfill match updates to a client of the spectator server.

---
+++ src/ggp/spectator/mapreduce/BackfillMapper.java
@@ -0,0 +1,56 @@
+package ggp.spectator.mapreduce;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.tools.mapreduce.AppEngineMapper;
+
+import org.apache.hadoop.io.NullWritable;
+
+import util.configuration.RemoteResourceLoader;
+
+import java.util.logging.Logger;
+
+public class BackfillMapper extends AppEngineMapper<Key, Entity, NullWritable, NullWritable> {
+  private static final Logger log = Logger.getLogger(BackfillMapper.class.getName());
+
+  public BackfillMapper() {
+      ;
+  }
+
+  @Override
+  public void taskSetup(Context context) {
+    log.warning("Doing per-task setup");
+  }
+
+  @Override
+  public void taskCleanup(Context context) {
+    log.warning("Doing per-task cleanup");
+  }
+
+  @Override
+  public void setup(Context context) {
+    log.warning("Doing per-worker setup");
+  }
+
+  @Override
+  public void cleanup(Context context) {
+    log.warning("Doing per-worker cleanup");    
+  }
+
+  // Quick observation map over the datastore, to collect numbers
+  // on how often various match descriptor fields are being used.
+  @Override
+  public void map(Key key, Entity value, Context context) {
+    log.warning("Mapping key: " + key);
+
+    try {
+        if (!key.getName().startsWith("ap") && !key.getName().startsWith("0")) {
+            String forgedAtomText = "<link href=\"http://matches.ggp.org/matches/" + key.getName() + "/\"/>";
+            RemoteResourceLoader.postRawWithTimeout("http://stats.ggp.org/ingestion/", forgedAtomText, 5000);
+        }
+        context.getCounter("Overall", "Readable").increment(1);
+    } catch (Exception e) {
+        context.getCounter("Overall", "Unreadable").increment(1);
+    }
+  }
+}
